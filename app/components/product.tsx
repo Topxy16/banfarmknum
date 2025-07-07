eceaac5d67d3da101e554a48e36403cc99d0c310
Thanaphols <cupteo254504@gmail.com>
2025-07-07 18:01:28 +0700

    DeleteProduct

--- app/components/product.tsx
+++ app/components/product.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import { io } from 'socket.io-client'
 
 import { useState } from 'react'
+import { useFormState } from 'react-dom'
 type propsProduct = {
     product : ProductType[]
     propsCategory : CategoryType []
@@ -30,16 +31,34 @@ export default function Product({ product , propsCategory }: propsProduct ) {
     const [showproduct_update, setShowProduct_update] = useState<boolean>(false)
     const [showproduct_dalete, setShowProduct_delete] = useState<boolean>(false)
     const [productUpdate , setProductUpdate] = useState<ProductType >()
+    const [p_ID , setProductID] = useState<number>(0)
     const HandleUpdate  = ()  => {
         setShowProduct_update(false)
-        
     }
-    const handleDelete = () => {
-
-        console.log('ลบสินค้าแล้ว')
-        setShowProduct_delete(false)
+    const handleDelete = async (ID:number) => {
+        const token = localStorage.getItem('token')
+        console.log(`ลบสินค้าแล้ว ${ID}`)
+        try {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products/deleteProduct/`+ID,{
+                method: 'DELETE',
+                headers: {
+                     Authorization: `Bearer ${token}`,
+                    'content-type': 'application/json'
+        },
+            })
+            const resData = await res.json()
+            if(resData.status ===  1){
+                console.log('Delete Product Successfully')
+            }else if (resData.status ===0){
+                console.log('Delete Product Unsuccessfully')
+            }
+            setShowProduct_delete(false)
+        } catch (error) {
+            console.log(error)
+            console.log('Delete Product Unsuccessfully')
+            setShowProduct_delete(false)
+        }
     }
-
     return (
         <div>
             <div>
@@ -61,7 +80,7 @@ export default function Product({ product , propsCategory }: propsProduct ) {
                                     </div>
                                     <div className='mt-2'>
                                         <div className="flex text-2xl place-content-end gap-1 mt-5">
-                                            <button className="bg-red-900 text-white rounded-xl p-2 hover:bg-red-700" id='del' onClick={() => setShowProduct_delete(true)}>
+                                            <button className="bg-red-900 text-white rounded-xl p-2 hover:bg-red-700" id='del' onClick={() => {setProductID(item.p_ID),setShowProduct_delete(true)}}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                 </svg>
@@ -90,7 +109,10 @@ export default function Product({ product , propsCategory }: propsProduct ) {
                                     show={showproduct_update}
                                     
                                     onClose={HandleUpdate} />
-                                <DeleteProduct
+                                
+                            </div>
+                ) }
+                    <DeleteProduct
                                     isOpen={showproduct_dalete}
                                     title="ลบสินค้า"
                                     message="คุณแน่ใจหรือไม่ว่าต้องการลบสินค้านี้?"
@@ -98,14 +120,11 @@ export default function Product({ product , propsCategory }: propsProduct ) {
                                     secondaryText="ยกเลิก"
                                     onConfirm={() => {
                                         // เรียก API ลบสินค้า
-                                        handleDelete()
+                                        handleDelete(p_ID)
                                         setShowProduct_delete(false)
                                     }}
                                     onSecondary={() => setShowProduct_delete(false)}
-                                />
-                            </div>
-                ) }
-                            
+                                />        
             </div>
         </div >
     )
