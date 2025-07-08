f772581652769e5a116a139fb5ca5a53a4fa18d3
Topxy16 <89091005+Topxy16@users.noreply.github.com>
2025-07-08 21:51:54 +0700

    cancel profile

--- app/components/aside.tsx
+++ app/components/aside.tsx
@@ -62,14 +62,7 @@ export default function aside() {
                   <span className="flex-1 ms-3 whitespace-nowrap">แผนที่</span>
                 </Link>
               </li> */}
-                        <li>
-                            <Link href="/profile" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-200 group">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                </svg>
-                                <span className="flex-1 ms-3 whitespace-nowrap">โปรไฟล์</span>
-                            </Link>
-                        </li>
+                        
                         {/* <li>
                 <Link href="#" className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-200 group">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
